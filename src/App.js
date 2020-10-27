1ba45a50dbaa2618d8ac3b5825db900a131e255c
Frenzy265 <klaus_franziska@gmx.de>
2020-10-27 15:48:34 +0100

    :sparkles:Introduce image preview

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import ImagePreview from "./components/imagePrevies";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <button>Get random image</button>
+      <ImagePreview
+        src={
+          "https://images.unsplash.com/photo-1525923838299-2312b60f6d69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80"
+        }
+        alt={"Leaves"}
+        author={"Leon"}
+      />
+    </main>
+  );
 }
 
 export default App;

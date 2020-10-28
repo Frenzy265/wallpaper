4f5d78d9ff9a02e55256fca27633f0ec7a844616
Frenzy265 <klaus_franziska@gmx.de>
2020-10-28 15:04:32 +0100

    Add storage function

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./ImagePreview.css";
+import IconButton from "./IconButton";
 
-export default function ImagePreview({ src, alt, author }) {
+export default function ImagePreview({ src, alt, author, id }) {
   return (
     <div className="imageContainer">
-      <button className="fav_btn">♥</button>
+      <IconButton
+        onClick={() => {
+          let favorites = null;
+          try {
+            favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+          } catch (error) {
+            // Parsing JSON might fail
+            console.error(error);
+            favorites = [];
+          }
+          if (favorites.includes(id)) {
+            // Already added to favorites
+            return;
+          }
+          const newFavorites = [...favorites, id];
+          localStorage.setItem("favorites", JSON.stringify(newFavorites));
+        }}
+      >
+        ⭑
+      </IconButton>
       <img className="imageThumb" src={src} alt={alt} />
       <p className="imageAuthor">Author: {author}</p>
     </div>
